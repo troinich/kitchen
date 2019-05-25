9d66c3fc23d07737052f53e160f80bdae53afde7
troinich <troinicz@gmail.com>
2019-05-25 20:37:14 +0200

    Fixed navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,43 +1,52 @@
-    <div class="jumbotron jumbotron-fluid text-center">
-        <div class="container">
-            <h2 class="display-4">The Kitchen.</h2>
-            <p class="lead"> Satisfy your hunger</p>
-        </div>
-    </div>
-
-<nav class="navbar-inverse">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="{{ route('blog.index') }}">The Kitchen</a>
-        </div>
-        <ul class="nav navbar-nav navbar-center">
-            <li><a href="{{ url('/category/asian') }}">Asian</a></li>
-            <li><a href="{{ url('/category/pasta') }}">Pasta</a></li>
-            <li><a href="{{ url('/category/vego') }}">Vego</a></li>
-            <li><a href="{{ url('/category/easy') }}">Easy</a></li>
-            <li><a href="{{ url('/category/children') }}">For kids</a></li>
-            <li><a href="{{ route('other.about') }}">Contact Us</a></li>
-            </ul>
-        <ul class="nav navbar-nav navbar-right">
+<!-- navbar -->
+<nav class="navbar navbar-expand-sm bg-light">
+    <div>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="navbar-brand" href="{{ route('blog.index') }}">The Kitchen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/category/asian') }}">Asian</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/category/pasta') }}">Pasta</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/category/vego') }}">Vego</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/category/easy') }}">Easy</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/category/children') }}">For kids</a>
+            </li>
             @if(!Auth::check())
-
-                <li><a href="{{ url('/login') }}">Login</a></li>
-                <li><a href="{{ url('/register') }}">Register</a></li>
-
+                <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}">Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/register') }}">Register</a></li>
             @else
-                <li><a href="{{ route('admin.index') }}">Admin</a></li>
-                <li>
-                    <a href="{{ url('/logout') }}"
-                       onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.index') }}">Admin</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/logout') }}"
+                                        onclick="event.preventDefault();
+               document.getElementById('logout-form').submit();">
                         Logout
-                    </a>
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
+                    </a></li>
+                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
             @endif
         </ul>
     </div>
 </nav>
+
+<div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+        <h2 class="display-4">The Kitchen.</h2>
+        <p class="lead"> Satisfy your hunger</p>
+    </div>
+</div>
+
+
+
+
+
 
